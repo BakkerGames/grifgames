64b6af66fecd115d379a80681c352240cc9885c8
Scott Bakker <sbakker343@gmail.com>
2026-09-15 19:37:21 -0400

    flag.dark = flag.15

--- advgames/walkthrough/adv01.sh
+++ advgames/walkthrough/adv01.sh
@@ -1,1 +1,2 @@
+rm ~/Documents/GRIF/adv01/*.*sav* 2>/dev/null
 ~/EXE/grif.exe adv01.grifstack -i walkthrough/adv01_walkthru.txt -o walkthrough/adv01_walkthru.log
--- advgames/walkthrough/adv02.sh
+++ advgames/walkthrough/adv02.sh
@@ -1,1 +1,2 @@
+rm ~/Documents/GRIF/adv02/*.*sav* 2>/dev/null
 ~/EXE/grif.exe adv02.grifstack -i walkthrough/adv02_walkthru.txt -o walkthrough/adv02_walkthru.log
